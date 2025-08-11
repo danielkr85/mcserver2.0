2e4c5e2bea3aa902c8bae0efa30419d883fa1f0a
danielkr85 <danielkr85@gmail.com>
2025-08-10 20:31:29 -0400

    worlds

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,13 +3,13 @@
 # Entrypoint script for Minecraft server
 # Usage: entrypoint.sh <min_memory> <max_memory> <world_dir> [additional_args]
 
-MIN_MEMORY="$1"
-MAX_MEMORY="$2"
-WORLD_DIR="$3"
+MIN_MEMORY="${MIN_MEMORY:-$1}"
+MAX_MEMORY="${MAX_MEMORY:-$2}"
+WORLD_DIR="${WORLD_DIR:-$3}"
 shift 3
 ADDITIONAL_ARGS="$@"
 
-JAR_FILE="server.jar"
+JAR_FILE="/mcserver/server.jar"
 
 if [ ! -f "$JAR_FILE" ]; then
   echo "Error: $JAR_FILE not found!"
@@ -21,4 +21,7 @@ if [ ! -d "$WORLD_DIR" ]; then
   mkdir -p "$WORLD_DIR"
 fi
 
-exec java -Xms${MIN_MEMORY} -Xmx${MAX_MEMORY} -jar "$JAR_FILE" --world "$WORLD_DIR" $ADDITIONAL_ARGS
+CMD="java -Xms${MIN_MEMORY} -Xmx${MAX_MEMORY} -jar $JAR_FILE --world ./worlds/$WORLD_DIR $ADDITIONAL_ARGS"
+echo "Executing: $CMD"
+exec $CMD
+
