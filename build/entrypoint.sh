3f7a078ebc3d5af07dcfd63c150e8c818e09bf16
danielkr85 <danielkr85@gmail.com>
2025-08-10 20:01:40 -0400

    ignore worlds

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # Entrypoint script for Minecraft server
-# Usage: entrypoint.sh <min_memory> <max_memory> [additional_args]
+# Usage: entrypoint.sh <min_memory> <max_memory> <world_dir> [additional_args]
 
 MIN_MEMORY="$1"
 MAX_MEMORY="$2"
-shift 2
+WORLD_DIR="$3"
+shift 3
 ADDITIONAL_ARGS="$@"
 
 JAR_FILE="server.jar"
@@ -15,4 +16,9 @@ if [ ! -f "$JAR_FILE" ]; then
   exit 1
 fi
 
-exec java -Xms${MIN_MEMORY} -Xmx${MAX_MEMORY} -jar "$JAR_FILE" $ADDITIONAL_ARGS
+if [ ! -d "$WORLD_DIR" ]; then
+  echo "World directory $WORLD_DIR not found. Creating it..."
+  mkdir -p "$WORLD_DIR"
+fi
+
+exec java -Xms${MIN_MEMORY} -Xmx${MAX_MEMORY} -jar "$JAR_FILE" --world "$WORLD_DIR" $ADDITIONAL_ARGS
